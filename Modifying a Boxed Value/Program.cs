4592ff737b912e36690f779352113bf7ad733654
Kulpot <kulpotwahu@gmail.com>
2023-08-25 12:11:35 +0800

    Add project files.

---
+++ Modifying a Boxed Value/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=Js1JN4D8igM&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=13
+//
+
+struct Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+class MainClass
+{
+    static void Main()
+    {
+        //object o = 5;
+        //object anotherO = 0;
+        //int temp = ((int)o);
+        //o = temp + 1;
+
+        //////////
+
+        Fraction myFrac = new Fraction();
+        object o = myFrac;
+        Fraction anotherFrac = (Fraction)myFrac;
+    }
+}
